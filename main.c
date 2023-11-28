378b9a5abaf6d496f44b813e907cb2f1d20e322e
nanameki1213 <nanameki1213@nanameki1213.github.com>
2023-11-28 20:56:05 +0900

    シンボルのアドレスをprintf

--- main.c
+++ main.c
@@ -4,45 +4,36 @@
 
 int main(void)
 {
-  char *str = "hello, world\n";
+  char buf[32];
 
-  printf("%d\n", 3);
-  // char buf[32];
+  INTR_ENABLE;
 
-  // INTR_ENABLE;
+  while(1) {
+    puts("$ ");
+    gets(buf);
+    if(!strncmp(buf, "echo", 4)) {
+      puts(buf + 4);
+      puts("\n");
+    } else if(!strcmp(buf, "exit")) {
+      break;
+    } else {
+      puts("unknown.\n");
+    }
+  }
 
-  // while(1) {
-  //   puts("$ ");
-  //   gets(buf);
-  //   if(!strncmp(buf, "echo", 4)) {
-  //     puts(buf + 4);
-  //     puts("\n");
-  //   } else if(!strcmp(buf, "exit")) {
-  //     break;
-  //   } else {
-  //     puts("unknown.\n");
-  //   }
-  // }
+  extern char text_start;
+  extern char etext;
+  extern char rodata_start;
+  extern char erodata;
+  extern char intrstack;
+  extern char bootstack;
 
-  // extern char text_start;
-  // extern char etext;
-  // extern char rodata_start;
-  // extern char erodata;
-  // extern char intrstack;
-  // extern char bootstack;
-
-  // puts("text_start: 0x");
-  // putxval(&text_start, 16);
-  // puts("etext: 0x");
-  // putxval(&etext, 16);
-  // puts("rodata_start: 0x");
-  // putxval(&rodata_start, 16);
-  // puts("erodata: 0x");
-  // putxval(&erodata, 16);
-  // puts("intrstack: 0x");
-  // putxval(&intrstack, 16);
-  // puts("bootstack: 0x");
-  // putxval(&bootstack, 16);
+  printf("test_start:   0x%x\n", &text_start);
+  printf("etext:        0x%x\n", &etext);
+  printf("rodata_start: 0x%x\n", &rodata_start);
+  printf("erodata:      0x%x\n", &erodata);
+  printf("intrstack:    0x%x\n", &intrstack);
+  printf("bootstack:    0x%x\n", &bootstack);
 
   return 0;
 }
