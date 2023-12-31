b6bde810e50dc68870af52f48cde3d6b495302ca
nanameki1213 <nanamekitaishin1213@icloud.com>
2023-12-31 12:53:53 +0900

    mie/mipレジスタのビット設定の間違いを修正

--- interrupt.c
+++ interrupt.c
@@ -1,16 +1,22 @@
 #include "interrupt.h"
 #include "riscv.h"
 
+// S，Mモード両方の割込みを有効にする
 void intr_enable()
 {
-  set_sie(get_sie() | xIE_xEIE | xIE_xTIE | xIE_xSIE);
-  set_mie(get_mie() | xIE_xEIE | xIE_xTIE | xIE_xSIE);
+  // ペンディングを表すレジスタをクリア
+  set_sip(0);
+  set_mip(0);
+
+  // set_sie(get_sie() | xIE_SEIE | xIE_STIE | xIE_SSIE);
+  set_mie(get_mie() | MIE_MEIE | MIE_MTIE | MIE_MSIE | xIE_SEIE | xIE_STIE | xIE_SSIE);
 }
 
+// S，Mモード両方の割込みを無効にする
 void intr_disable()
 {
-  set_sie(get_sie() & ~(xIE_xEIE | xIE_xTIE | xIE_xSIE));
-  set_mie(get_mie() & ~(xIE_xEIE | xIE_xTIE | xIE_xSIE));
+  // set_sie(get_sie() & ~(xIE_SEIE | xIE_STIE | xIE_SSIE));
+  set_mie(get_mie() & ~(MIE_MEIE | MIE_MTIE | MIE_MSIE | xIE_SEIE | xIE_STIE | xIE_SSIE));
 }
 
 void interrupt(softvec_type_t type, unsigned int sp)
--- riscv.h
+++ riscv.h
@@ -4,13 +4,17 @@
 #define INTR_SOURCES_NUM 96
 #define INTR_PRIORITY_MAX 7
 
-#define xIE_xEIE (1<<9) // 外部割込み許可
-#define xIE_xTIE (1<<5) // タイマ割込み許可
-#define xIE_xSIE (1<<1) // ソフトウェア割込み許可
+#define MIE_MEIE (1<<11) // 外部割込み許可
+#define MIE_MTIE (1<<7) // タイマ割込み許可
+#define MIE_MSIE (1<<3) // ソフトウェア割込み許可
 
-#define xIP_xEIP (1<<9) // 外部割込みペンディング
-#define xIP_xTIP (1<<5) // タイマ割り込みペンディング
-#define xIP_xSIP (1<<1) // ソフトウェア割込みペンディング
+#define xIE_SEIE (1<<9) // 外部割込み許可(Sモード)
+#define xIE_STIE (1<<5) // タイマ割込み許可(Sモード)
+#define xIE_SSIE (1<<1) // ソフトウェア割込み許可(Sモード)
+
+#define MIP_xEIP (1<<9) // 外部割込みペンディング
+#define MIP_xTIP (1<<5) // タイマ割り込みペンディング
+#define MIP_xSIP (1<<1) // ソフトウェア割込みペンディング
 
 #define MODE_VECTOR 1U
 #define MODE_DIRECT 0U
@@ -68,25 +72,50 @@ static inline void set_stvec(int x)
 static inline int get_sie()
 {
   int x;
-  asm volatile("csrr %0, sip" : "=r"(x));
+  asm volatile("csrr %0, sie" : "=r"(x));
   return x;
 }
 
 static inline void set_sie(int x)
 {
-  asm volatile("csrw sip, %0" : "=r"(x));
+  asm volatile("csrw sie, %0" : "=r"(x));
 }
 
 static inline int get_mie()
+{
+  int x;
+  asm volatile("csrr %0, mie" : "=r"(x));
+  return x;
+}
+
+static inline void set_mie(int x)
+{
+  asm volatile("csrw mie, %0" : "=r"(x));
+}
+
+static inline void get_sip()
+{
+  int x;
+  asm volatile("csrr %0, sip" : "=r"(x));
+  return x;
+}
+
+static inline void set_sip(int x)
+{
+  asm volatile("csrw sip, %0" : "=r"(x));
+}
+
+static inline void get_mip()
 {
   int x;
   asm volatile("csrr %0, mip" : "=r"(x));
   return x;
 }
 
-static inline void set_mie(int x)
+static inline void set_mip(int x)
 {
   asm volatile("csrw mip, %0" : "=r"(x));
 }
+
 
 #endif
